6ffdc7646ec2555cf7bf8987be6492a8fbe69ea8
marcomull <105877026+marcomull@users.noreply.github.com>
2024-10-01 11:52:26 -0500

    creating project

---
+++ src/main/java/org/example/finalworkapi/FinalWorkApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.finalworkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalWorkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalWorkApiApplication.class, args);
+    }
+
+}
