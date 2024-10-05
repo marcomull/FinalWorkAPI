a5702b66afce6d792a280418bed78ffabb1b1215
marcomull <105877026+marcomull@users.noreply.github.com>
2024-10-04 19:49:12 -0500

    creating the classes

---
+++ src/main/java/org/example/finalworkapi/Domain/Entities/Driver.java
@@ -0,0 +1,2 @@
+package org.example.finalworkapi.Domain.Entities;public class driver {
+}
---
+++ src/main/java/org/example/finalworkapi/Domain/Entities/SparPart.java
@@ -0,0 +1,14 @@
+package org.example.finalworkapi.Domain.Entities;
+
+import java.util.Date;
+
+public class Replacement {
+
+    protected int idRepuesto;
+    protected int idLogistics;
+    protected Date arrivalDate;
+    protected String replacement;
+    protected int cantidad;
+
+    //
+}
