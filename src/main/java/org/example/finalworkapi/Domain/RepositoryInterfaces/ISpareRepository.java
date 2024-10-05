7e53d5cd4d5fbd065ccd64b176f1334b3475fa1c
marcomull <105877026+marcomull@users.noreply.github.com>
2024-10-05 08:26:59 -0500

    Code

--- src/main/java/org/example/finalworkapi/Domain/RepositoryInterfaces/ISpareRepository.java
+++ src/main/java/org/example/finalworkapi/Domain/RepositoryInterfaces/ISpareRepository.java
@@ -1,9 +1,9 @@
 package org.example.finalworkapi.Domain.RepositoryInterfaces;
 
-import org.example.finalworkapi.Domain.Entities.SparPart;
+import org.example.finalworkapi.Domain.Entities.SparePart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ISpareRepository extends JpaRepository<SparPart, Integer> {
+public interface ISpareRepository extends JpaRepository<SparePart, Integer> {
 }
