5a0963aad21bdffcdf06d83735eda49420847c5b
unknown <juantdtd@gmail.com>
2024-10-10 10:52:52 -0500

    Creating DTO of Sparking parts and mappers

--- src/main/java/org/example/finalworkapi/Application/Mappers/SparePartRequestMapper.java
+++ src/main/java/org/example/finalworkapi/Application/Mappers/SparePartRequestMapper.java
@@ -1,4 +1,19 @@
 package org.example.finalworkapi.Application.Mappers;
 
+import org.example.finalworkapi.Application.DTOs.SparePartRequestDTO;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
 public class SparePartRequestMapper {
+
+    public SparePartRequestDTO toDTO(Object[] maintenanceData) {
+        SparePartRequestDTO dto = new SparePartRequestDTO();
+        dto.setIdSparePart((Integer) maintenanceData[0]);
+        dto.setArrivalDate((Date) maintenanceData[1]);
+        dto.setSparePart((String) maintenanceData[2]);
+        dto.setQuantity((Integer) maintenanceData[3]);
+        return dto;
+    }
+
+
 }
